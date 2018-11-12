0eac146342fab71047571dbbb8be3c25dd0fc42c
Rodrigo Diaz <esteban.diaz@globant.com>
2018-11-11 22:29:51 -0300

    Servicio con contenido estatico

--- my-first-server.js
+++ my-first-server.js
@@ -1,12 +1,29 @@
 //Cargando modulo http
 var http = require("http");
+var fileSystem = require("fs");
 const SC_OK = 200;
+const SC_NOT_FOUND = 404;
 
 //Servidor 'hola mundo'
 var server = http.createServer(
   function(rq, rs) {
-    rs.writeHead(SC_OK, {"Content_Type": "text/html"});
-    rs.end("<h1>Hola Mundo!</h1>");
+    console.log(rq.url);
+
+    if(rq.url === "/") {
+      fileSystem.readFile("index.html", function(err, data) {
+        rs.writeHead(SC_OK, {"Content_Type": "text/html"});
+        rs.end(data);
+      });
+    } else
+    if(rq.url === "/img/node-logo.png") {
+      fileSystem.readFile("./img/node-logo.png", function(err, data) {
+        rs.writeHead(SC_OK, {"Content_Type": "image/png"});
+        rs.end(data);
+      });
+    } else {
+      rs.writeHead(SC_OK, {"Content_Type": "text/html"});
+      rs.end("<h1>Pagina no encontrada</h1>");
+    }
   }
 );
 
